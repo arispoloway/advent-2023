9afc942a622f5ca54901214494e100fe45e6078f
Aris Poloway <agpoloway@gmail.com>
2023-12-11 09:05:21 -0500

    Day 11

---
+++ solutions/day11/main.py
@@ -0,0 +1,53 @@
+import math
+from solutions.util import read_input
+
+
+SPACE = "."
+GALAXY = "#"
+
+def parse(lines):
+    emptyXs = {}
+    emptyYs = {}
+    emptyXCount = 0
+    emptyYCount = 0
+
+    for y, line in enumerate(lines):
+        if all(c == SPACE for c in line):
+            emptyYCount += 1
+        emptyYs[y] = emptyYCount
+
+
+    for col in range(len(lines[0])):
+        if all(c == SPACE for c in map(lambda x: x[col], lines)):
+            emptyXCount += 1
+        emptyXs[col] = emptyXCount
+
+    galaxies = []
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == GALAXY:
+                galaxies.append((x, y))
+
+    return galaxies, emptyXs, emptyYs
+
+
+def calculateExpansion(lines, expansionFactor):
+    galaxies, emptyXs, emptyYs = parse(lines)
+
+    s = 0
+    for g1 in galaxies:
+        for g2 in galaxies:
+            s += abs(g1[0] - g2[0]) + expansionFactor * abs(emptyXs[g1[0]] - emptyXs[g2[0]])
+            s += abs(g1[1] - g2[1]) + expansionFactor * abs(emptyYs[g1[1]] - emptyYs[g2[1]])
+
+    return int(s / 2)
+
+
+def part1(lines):
+    return calculateExpansion(lines, 1)
+def part2(lines):
+    return calculateExpansion(lines, 999999)
+
+lines = read_input(11)
+print(part1(lines))
+print(part2(lines))
